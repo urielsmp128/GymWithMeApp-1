26ddbcacae1c7ef138265abac5692885c382f63e
Alberto Labrada <albertolabrada99@gmail.com>
2020-06-04 20:27:46 -0600

    navigation setup

--- app/src/main/java/com/itesm/gymwithme/ui/HomeFragment.java
+++ app/src/main/java/com/itesm/gymwithme/ui/HomeFragment.java
@@ -8,10 +8,16 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
+import com.google.android.material.card.MaterialCardView;
 import com.itesm.gymwithme.R;
 
 public class HomeFragment extends Fragment {
+
+    private MaterialCardView cardViewWorkout;
+    private MaterialCardView cardViewScan;
+    private MaterialCardView cardViewConnect;
 
     @Nullable
     @Override
@@ -20,6 +26,22 @@ public class HomeFragment extends Fragment {
             @Nullable ViewGroup container,
             @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        cardViewWorkout = view.findViewById(R.id.card_workout);
+        cardViewScan = view.findViewById(R.id.card_scan);
+        cardViewConnect = view.findViewById(R.id.card_connect);
+
+        cardViewWorkout.setOnClickListener(v ->
+                Navigation.findNavController(view).navigate(R.id.action_nav_home_to_routineListFragment));
+
+        cardViewScan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_nav_home_to_cameraXActivity);
+            }
+        });
+
+        return view;
     }
 }
